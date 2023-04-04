70d8ac4112110cf0cbc5d8ce319fae1f0ac7e9ee
mateus gabriel barbosa <mateus_gabriel96@outlook.com>
2023-04-04 16:04:15 -0300

    Aulas secao 4: angular

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss'],
+})
+export class EventosComponent implements OnInit {
+  public eventos: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:7082/api/evento').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+    // this.eventos = [
+    //   {
+    //     Tema: 'Angular',
+    //     Local: 'São Paulo',
+    //   },
+    //   {
+    //     Tema: '.NET',
+    //     Local: 'Cascavel',
+    //   },
+    //   {
+    //     Tema: 'Angular com .NET',
+    //     Local: 'Campinas',
+    //   },
+    // ];
+  }
+}
