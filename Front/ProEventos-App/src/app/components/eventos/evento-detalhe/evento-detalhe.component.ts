4ee060a0739dfe4f89aeb9aff5bb3f9b4a19c1a5
mateus gabriel barbosa <mateus_gabriel96@outlook.com>
2023-09-13 13:09:55 -0300

    formulario sem formBuilder

--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
   templateUrl: './evento-detalhe.component.html',
-  styleUrls: ['./evento-detalhe.component.scss']
+  styleUrls: ['./evento-detalhe.component.scss'],
 })
 export class EventoDetalheComponent implements OnInit {
+  // form!: FormGroup;
+  form: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.validation();
   }
 
+  public validation(): void {
+    this.form = new FormGroup({
+      tema: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(50),
+      ]),
+      local: new FormControl('', Validators.required),
+      dataEvento: new FormControl('', Validators.required),
+      quantidadePessoas: new FormControl('', [
+        Validators.required,
+        Validators.max(120000),
+      ]),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      imagemURL: new FormControl('', Validators.required),
+    });
+  }
 }
